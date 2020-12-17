033f694c1e24755e6e4fb4d71a4fb4d803f5d5a5
anhnbt-it <anhnbt.it@gmail.com>
2020-12-17 11:54:16 +0700

    Add interface Service

--- src/main/java/com/codegym/service/CustomerService.java
+++ src/main/java/com/codegym/service/CustomerService.java
@@ -1,4 +1,15 @@
 package com.codegym.service;
 
-public class CustomerService {
+import com.codegym.model.Customer;
+
+import java.util.Optional;
+
+public interface CustomerService {
+    Iterable<Customer> findAll();
+
+    Optional<Customer> findById(Long id);
+
+    void save(Customer customer);
+
+    void remove(Long id);
 }
